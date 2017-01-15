fe7d9f762f63791fd93b0cd007d55f927927982e
donikv <donikv@gmail.com>
2017-01-15 11:28:52 +0100

    kombe s userima i pocetnom stranicom

--- src/GalleryApplication/Controllers/HomeController.cs
+++ src/GalleryApplication/Controllers/HomeController.cs
@@ -1,16 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel.Configuration;
 using System.Threading.Tasks;
+using ApplicationClasses;
+using GalleryApplication.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GalleryApplication.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly List<User> _users;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public HomeController(List<User> users,
+        UserManager<ApplicationUser> userManager)
         {
-            return View();
+            _userManager = userManager;
+            _users = users;
+        }
+        [Authorize]
+        public async Task<IActionResult> Index()
+        {
+            ApplicationUser currentApplicationUserUser = await _userManager.GetUserAsync(HttpContext.User);
+            var currentUser = _users.Find(s => s.Id == (Guid.Parse(currentApplicationUserUser.Id)));
+            return View(currentUser);
         }
 
         public IActionResult About()
