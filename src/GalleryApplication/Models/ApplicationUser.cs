d13df5c6b3fa68f2c62e54b881780b916dadf091
sodic <filip.sodic@gmail.com>
2017-01-16 12:32:08 +0100

    deri

--- src/GalleryApplication/Models/ApplicationUser.cs
+++ src/GalleryApplication/Models/ApplicationUser.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ApplicationClasses.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace GalleryApplication.Models
@@ -9,5 +10,6 @@ namespace GalleryApplication.Models
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        private User user;
     }
 }
